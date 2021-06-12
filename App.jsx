d9f8e718350aca023188139c29730d836e70cb6a
Jatin Sharma <44229425+jshrma14@users.noreply.github.com>
2021-06-12 22:34:30 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from "./container/Header";
+import Footer from "./container/Footer";
+import Notes from "./container/Note";
+
+function App(){
+    return (
+        <div>
+        <Header />
+        <Notes />
+        <Footer />
+        </div>
+
+    );
+}
+
+export default App;
---
+++ Note.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Notes(){
+    return (
+    <div className="note">
+        <h1>First Note</h1>
+        <p>Just a sample.</p>
+    </div>);
+}
+
+export default Notes;
